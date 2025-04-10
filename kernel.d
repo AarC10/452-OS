2ad6723e3abc4ccd612646b2afc94a49c14b5b31
Aaron Chan <aarchan108@gmail.com>
2025-04-10 16:08:02 -0400

    Add gitignore

--- kernel.d
+++ kernel.d
@@ -1,6 +1,6 @@
-kernel.o: kernel.c include/bootstrap.h include/cio.h include/clock.h \
- include/common.h include/types.h include/params.h include/defs.h \
- include/kdefs.h include/debug.h include/support.h include/lib.h \
- include/klib.h include/x86/ops.h include/kmem.h include/procs.h \
+kernel.o: kernel.c include/common.h include/types.h include/params.h \
+ include/defs.h include/kdefs.h include/debug.h include/cio.h \
+ include/support.h include/lib.h include/klib.h include/x86/ops.h \
+ include/bootstrap.h include/clock.h include/kmem.h include/procs.h \
  include/sio.h include/compat.h include/syscalls.h include/user.h \
- include/x86/arch.h include/vm.h include/drivers/intel_8255x.h
+ include/x86/arch.h include/vm.h
