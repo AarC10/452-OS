db9c9e54564bbaa2ca5f8ca5ad55362bdc4c9f04
Aaron Chan <aarchan108@gmail.com>
2025-05-02 02:06:16 -0400

    Define basic functions for 8255x header

--- include/drivers/intel8255x.h
+++ include/drivers/intel8255x.h
@@ -3,4 +3,12 @@
 
 #include <types.h>
 
+int i8255x_init(uint32_t pci_bar, bool is_io);
+
+int i8255x_transmit(const uint8_t *frame, uint16_t len);
+
+int i8255x_receive(uint8_t *buf, uint16_t bufsize);
+
+void i8255x_get_mac(uint8_t mac_out[6]);
+
 #endif // _INTEL8255X_DRIVER_
