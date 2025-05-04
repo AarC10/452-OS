477720d1e4d1186a41ab7af0730925edfc1db458
Will Bicks <wab3463@rit.edu>
2025-05-03 23:58:23 -0400

    Use struct for accessing mmio fields.

--- hda.c
+++ hda.c
@@ -7,31 +7,45 @@
 #include <pci.h>
 #include <hda.h>
 
-#define HDA_REG_GCAP 0x00
-#define HDA_REG_VMIN 0x02
-#define HDA_REG_VMAJ 0x03
-#define HDA_REG_GCTL 0x08
-
 // Macros for handling memory mapped IO with byte offsets
 #define MMIO_BYTE(ptr, offset) 	((uint8_t*)((char*)(ptr) + (offset)))
 #define MMIO_HW(ptr, offset) 	((uint16_t*)((char*)(ptr) + (offset)))
 #define MMIO_WORD(ptr, offset) 	((uint32_t*)((char*)(ptr) + (offset)))
 
+#define HDA_GCTL_CRST 	0x1	// Controller reset
+
+// HDA Audio Controller register set
+// See: High Definition Audio Specificaiton section 3.3
+#pragma pack(push, 1)
+typedef struct hda_regset {
+	uint16_t 	gcap; 	// Global capabilities
+	uint8_t		vmin;	// Minor version
+	uint8_t 	vmaj; 	// Major version
+	uint16_t	outpay;	// Output payload capability
+	uint16_t	inpay;	// Input payload capability
+
+	uint32_t 	gctl; 	// Global control
+	uint16_t 	wakeen;	// Wake enable
+	uint16_t	wakests;// Wake status
+	uint16_t	gsts;	// Global status
+
+	uint32_t	rsvd12;	// Reserved
+	uint8_t		rsvd17;	// Reserved
+
+	uint16_t 	outstrmpay;	// Output stream payload capability
+	uint16_t 	instrmpay;	// Input stream payload capability
+} hda_regset_t;
+#pragma pack(pop)
+
 // HDA controller
 typedef struct hda {
-	void* bar;
+	hda_regset_t* regs;
 } hda_t;
 
 void print_controller_info(hda_t* hda) {
-	uint8_t vmin, vmaj;
+	cio_printf("hda: controller v%d.%d\n", hda->regs->vmaj, hda->regs->vmin);
 
-	vmin = *MMIO_BYTE(hda->bar, HDA_REG_VMIN);
-	vmaj = *MMIO_BYTE(hda->bar, HDA_REG_VMAJ);
-
-	cio_printf("hda: controller v%d.%d\n", vmaj, vmin);
-
-	uint16_t caps = *MMIO_HW(hda->bar, HDA_REG_GCAP);
-	cio_printf("hda: capabilties 0x%x\n", caps);
+	cio_printf("hda: capabilties 0x%x\n", hda->regs->gcap);
 }
 
 void hda_init() {
@@ -47,18 +61,22 @@ void hda_init() {
 		return;
 	}
 
-	// Bring HDA device out of reset
-	// outl(io_base + HDA_REG_GCTL, HDA_GCTL_CRST);
+	hda_regset_t* regs = (hda_regset_t*) io_base;
 
-	// Wait for CRST to be set (controller ready)
-	// uint32_t gctl;
-	// do {
-	//     delay(1);
-	//     gctl = inl(io_base + HDA_REG_GCTL);
-	// } while (!(gctl & HDA_GCTL_CRST));
+	// Bring the controller into and out of reset
+	regs->gctl &= ~HDA_GCTL_CRST;
+	do {
+		delay(1);
+	} while(regs->gctl && HDA_GCTL_CRST);
+	delay(1);
+	regs->gctl |= HDA_GCTL_CRST;
+	do {
+		delay(1);
+	} while(!(regs->gctl && HDA_GCTL_CRST));
+	delay(1); // Additional delay for codecs to reset
 
 	hda_t hda = {
-		.bar = io_base
+		.regs = (hda_regset_t*) io_base
 	};
 
 	print_controller_info(&hda);
