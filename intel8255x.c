a8ff6fd3b7bea90fb984e16f337996fe8522cae4
Aaron Chan <aarchan108@gmail.com>
2025-05-05 14:45:47 -0400

    mmio register access fun

--- intel8255x.c
+++ intel8255x.c
@@ -2,6 +2,14 @@
 #include <types.h>
 #include <drivers/intel8255x_ops.h>
 
+static uint_t read_reg(i8255x *dev, uint32_t offset) {
+    return *(volatile uint32_t *)(dev->mmio_base + offset);
+}
+
+static void write_reg(i8255x *dev, uint32_t offset, uint32_t value) {
+    *(volatile uint32_t *)(dev->mmio_base + offset) = value;
+}
+
 int i8255x_init(uint32_t pci_bar, bool_t is_io) { return -1; }
 
 int i8255x_transmit(const uint8_t *frame, uint16_t len) { return -1; }
