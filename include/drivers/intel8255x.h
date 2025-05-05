63de0fb5bdb1bd8ad50753087416939693b021ba
Aaron Chan <aarchan108@gmail.com>
2025-05-05 14:44:29 -0400

    Fix naming

--- include/drivers/intel8255x.h
+++ include/drivers/intel8255x.h
@@ -3,11 +3,11 @@
 
 #include <types.h>
 
-#define E1000_RX_RING_SIZE 16
-#define E1000_TX_RING_SIZE 16
+#define I8255X_RX_RING_SIZE 16
+#define I8255X_TX_RING_SIZE 16
 
-#define E1000_RXD_STAT_DD 0x01  // Desc done
-#define E1000_RXD_STAT_EOP 0x02 // End of packet
+#define I8255X_RXD_STAT_DD 0x01  // Desc done
+#define I8255X_RXD_STAT_EOP 0x02 // End of packet
 
 typedef struct {
     uint64_t addr; // Addr of the data buffer
@@ -17,7 +17,7 @@ typedef struct {
     uint8_t status; // Desc status
     uint8_t css; // Checksum Start
     uint16_t special; // Special field
-} e1000_tx_desc;
+} i8255x_tx_desc;
 
 typedef struct {
     uint64_t addr;   // Addr of data buffer
@@ -26,17 +26,17 @@ typedef struct {
     uint8_t status;  // Desc status
     uint8_t errors;  // Desc errors
     uint16_t special; // Special field
-} e1000_rx_desc;
+} i8255x_rx_desc;
 
 typedef struct {
     uint32_t mmio_base;
-    e1000_rx_desc rx_ring[E1000_RX_RING_SIZE] __attribute__((aligned(16)));
-    e1000_tx_desc tx_ring[E1000_TX_RING_SIZE] __attribute__((aligned(16)));
+    i8255x_rx_desc rx_ring[I8255X_RX_RING_SIZE] __attribute__((aligned(16)));
+    i8255x_tx_desc tx_ring[I8255X_TX_RING_SIZE] __attribute__((aligned(16)));
     uint8_t addr[6];
     uint8_t irq;
     struct netdev *netdev;
-    struct e1000 *next;
-} e1000;
+    struct i8255x *next;
+} i8255x;
 
 int i8255x_init(uint32_t pci_bar, bool_t is_io);
 
