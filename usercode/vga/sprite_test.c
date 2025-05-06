fe4e591508ec294572aefa5d54b11538d03f231e
Nicholas Merante <ncm2705@rit.edu>
2025-05-05 21:58:08 -0400

    Update smiley to be 16x16 instead of 8x8 sprite

--- usercode/vga/sprite_test.c
+++ usercode/vga/sprite_test.c
@@ -36,16 +36,38 @@ static const uint8_t checker8[8 * 8] = {
     1, 0, 1, 0, 1, 0, 1, 0,
     0, 1, 0, 1, 0, 1, 0, 1};
 
+static const uint8_t smiley16[16 * 16] = {
+    0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0,
+    0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0,
+    0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0,
+    0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0,
+    0, 1, 1, 1, 0, 0, 1, 1, 1, 1, 0, 0, 1, 1, 1, 0,
+    1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1,
+    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+    1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1,
+    1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1,
+    1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1,
+    0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0,
+    0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0,
+    0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0,
+    0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0,
+    0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0
+};
+    
+
 USERMAIN(sprite_test)
 {
     swrites("sprite_test: bouncing checker with delays a...\n");
 
+    // Sprite dimensions
+    const int w = 16, h = 16;
+
     // Start near center
-    int x = (VGA_WIDTH - 8) / 2;
-    int y = (VGA_HEIGHT - 8) / 2;
+    int x = (VGA_WIDTH - w) / 2;
+    int y = (VGA_HEIGHT - h) / 2;
     int dx = 2;
     int dy = 1;
-    const int w = 8, h = 8;
     int toggle = 0;
     vga_set_palette_color(1, 0xFF, 0xFF, 0x00); // Yellow (RGB: 255, 255, 0)
 
@@ -57,7 +79,7 @@ USERMAIN(sprite_test)
         vga_clear_buf(VGA_COLOR_LIGHT_BLUE);
 
         // Draw sprite
-        vga_blit(smiley8, w, h, x, y, 0);
+        vga_blit(smiley16, w, h, x, y, 0);
         vga_render();
 
         // Update position and bounce
@@ -69,7 +91,7 @@ USERMAIN(sprite_test)
             dy = -dy;
 
         // Wait to set movement speed
-        vga_sleep_ms(6);
+        vga_sleep_ms(8);
     }
 
     return 0;
